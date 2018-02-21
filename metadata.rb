24905587f5d3c4014ce52e5ab5e7e9e620910a80
Gene Gotimer <gene.gotimer@coveros.com>
2018-02-21 07:14:44 -0800

    Fixed supports platform version

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,5 @@ license 'All Rights Reserved'
 description 'Installs Node.js on Windows'
 long_description 'Installs Node.js on Windows'
 version '0.1.0'
-supports 'windows', '2012R2'
-supports 'windows', '2016'
+supports 'windows'
 chef_version '>= 12.1' if respond_to?(:chef_version)
